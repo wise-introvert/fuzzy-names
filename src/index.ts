aba3a01ab4c6af97fdcb0ae8c836f708764ceb96
Fardeen Panjwani <edmond@montecristo.com>
2024-07-20 16:50:19 +0530

    fix: add export statement for match-metric

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './normalize';
 export * from './split';
 export * from './calculate-lev-distance';
 export * from './calculate-phonetics-metric';
+export * from './calculate-match-metric';
