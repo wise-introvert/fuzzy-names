159c45306a9793266295681e7a08c6edbafb2f6e
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2025-03-14 17:12:46 +0530

    wip: build: optimize bundle size using rollup

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true,
+      exports: 'named',
+    },
+  ],
+  external: [
+    'lodash.deburr',
+    'lodash.every',
+    'lodash.get',
+    'lodash.isempty',
+    'lodash.negate',
+    'lodash.orderby',
+    'natural',
+    'tslib'
+  ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: './dist',
+      include: ['src/**/*'],
+      exclude: ['**/*.test.ts'],
+    }),
+    terser({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+      }
+    })
+  ]
+}; 
